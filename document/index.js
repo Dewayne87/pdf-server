3b27eee6fe271d455e030faae706e038d282f554
Dewayne87 <dewaynelindsay87@gmail.com>
2019-05-08 04:33:16 -0700

    changes

--- document/index.js
+++ document/index.js
@@ -15,41 +15,41 @@ module.exports = ({
   shipping,
   total
 }) => {
-        const headerellipsis = str => {
-          return str.length > 10 ? str.slice(0, 11) : str;
-        };
-        const itemChecker = items => {
-          let emptyItems = [
-            {
-              name: "",
-              description: "",
-              cost: "",
-              quantity: "",
-              amount: ""
-            }
-          ];
-          return items ? items : emptyItems;
-        };
+  headerellipsis = str => {
+    return str.length > 10 ? str.slice(0, 11) : str;
+  };
+  const itemChecker = items => {
+    let emptyItems = [
+      {
+        name: "",
+        description: "",
+        cost: "",
+        quantity: "",
+        amount: ""
+      }
+    ];
+    return items ? items : emptyItems;
+  };
 
-        const capitalizeFirstLetter = str => {
-          return str.charAt(0).toUpperCase() + str.slice(1);
-        };
-        let taxpercent = Number(tax) * 100;
-        // const invoiceItems = items.map(row => {
-        //   const { name, description, cost, quantity, amount} = row;
-        //   return (
-        //     `<tr class='invoice-item'>
-        //       <td>${name}</td>
-        //       <td>${description}</td>
-        //       <td>${cost}</td>
-        //       <td>${quantity}</td>
-        //       <td>${amount}</td>
-        //     </tr>`
-        //   )
-        // })
-        //   add items const to line 272 when items array working like this ->>//   ${items.join()}
+  capitalizeFirstLetter = str => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
+  let taxpercent = Number(tax) * 100;
+  // const invoiceItems = items.map(row => {
+  //   const { name, description, cost, quantity, amount} = row;
+  //   return (
+  //     `<tr class='invoice-item'>
+  //       <td>${name}</td>
+  //       <td>${description}</td>
+  //       <td>${cost}</td>
+  //       <td>${quantity}</td>
+  //       <td>${amount}</td>
+  //     </tr>`
+  //   )
+  // })
+  //   add items const to line 272 when items array working like this ->>//   ${items.join()}
 
-        return `
+  return `
  <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -108,43 +108,43 @@ module.exports = ({
     </style>
   </head>
   <body>
-    <div className="container">
-    <div className="appbar"><h3>${company.name}</h3></div>
-<div className="box-container bottom">
-            <div className="top-box box">
+    <div class="container">
+    <div class="appbar"><h3>${capitalizeFirstLetter(company.name)}</h3></div>
+<div class="box-container bottom">
+            <div class="top-box box">
               <p>
                 <strong>
-                  <span className="entryName">Invoice #:</span>
+                  <span class="entryName">Invoice #:</span>
                 </strong>
                 ${" " + number}
                 <br />
                 <strong>
-                  <span className="entryName">Date:</span>
+                  <span class="entryName">Date:</span>
                 </strong>
                 ${headerellipsis(" " + date)}
                 <br />
                 <strong>
-                  <span className="entryName">Due Date:</span>
+                  <span class="entryName">Due Date:</span>
                 </strong>
                 ${headerellipsis(" " + dueDate)}
                 <br />
                 <strong>
-                  <span className="entryName">Total:</span>
+                  <span class="entryName">Total:</span>
                 </strong>
                 ${" $" + total}
                 <br />
                 <strong>
-                  <span className="entryName">Amount Due:</span>
+                  <span class="entryName">Amount Due:</span>
                 </strong>
                 ${" $" + balance}
               </p>
             </div>
           </div>
-          <div className="box-container bottom">
-            <div className="box">
+          <div class="box-container bottom">
+            <div class="box">
               <p>
                 <strong>
-                  <span className="entryName">From:</span>
+                  <span class="entryName">From:</span>
                 </strong>
                 <br />
                 ${capitalizeFirstLetter(company.name)}
@@ -160,10 +160,10 @@ module.exports = ({
                 ${" " + company.phoneNumber}
               </p>
             </div>
-            <div className="box">
+            <div class="box">
               <p>
                 <strong>
-                  <span className="entryName">To:</span>
+                  <span class="entryName">To:</span>
                 </strong>
                 <br />
                 ${capitalizeFirstLetter(customer.name)}
@@ -179,50 +179,42 @@ module.exports = ({
               </p>
             </div>
           </div>
-          <div className="box-container bottom">
-            <div className="box mobileBorder">
+          <div class="box-container bottom">
+            <div class="box mobileBorder">
               <p>
                 <strong>
-                  <span className="entryName">Invoice Description:</span>
+                  <span class="entryName">Invoice Description:</span>
                 </strong>
                 <br />
                 ${description + "."}
               </p>
             </div>
-            <div className="box">
-              <p className="subtotalTax">Subtotal: $${
-                subtotal
-              }</p>
-              <p className="shippingDiscount">Discount: $${
-                discount
-              }</p>
-              <p className="subtotalTax">
+            <div class="box">
+              <p class="subtotalTax">Subtotal: $${subtotal}</p>
+              <p class="shippingDiscount">Discount: $${discount}</p>
+              <p class="subtotalTax">
                 Tax:
                 ${" " + Number(tax) * 100}%
               </p>
-              <p className="shippingDiscount">Shipping: $${
-                shipping
-              }</p>
-              <p className="total-due">Total: $${total}</p>
-              <p className="amount-paid">Balance: $${
-                balance
-              }</p>
+              <p class="shippingDiscount">Shipping: $${shipping}</p>
+              <p class="total-due">Total: $${total}</p>
+              <p class="amount-paid">Balance: $${balance}</p>
             </div>
           </div>
-          <div className="box-container">
-            <div className="box">
+          <div class="box-container">
+            <div class="box">
               <p>
                 <strong>
-                  <span className="entryName">Notes (if applicable):</span>{" "}
+                  <span class="entryName">Notes (if applicable):</span>{" "}
                 </strong>
                 <br />
                 ${notes + "."}
               </p>
             </div>
-            <div className="box">
+            <div class="box">
               <p>
                 <strong>
-                  <span className="entryName">Terms (if applicable):</span>
+                  <span class="entryName">Terms (if applicable):</span>
                 </strong>
                 <br />
                 ${terms + "."}
@@ -230,7 +222,7 @@ module.exports = ({
             </div>
           </div>
 
-    <div className="appbar"><h3>Invoice Items (if applicable)</h3></div>
+    <div class="appbar"><h3>Invoice Items (if applicable)</h3></div>
             <table>
             <tr>
                 <td>Name</td>
@@ -244,4 +236,4 @@ module.exports = ({
   </body>
 </html>
   `;
-      };
+};
