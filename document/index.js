804c3ab5add5f38d05de9619b989ae84dcef1ace
Dewayne87 <dewaynelindsay87@gmail.com>
2019-05-08 22:38:44 -0700

    final draft

--- document/index.js
+++ document/index.js
@@ -131,7 +131,7 @@ body{
 
 .box{
    width:50%;
-   min-height:170px;
+   min-height:180px;
    padding-top:15px;
    font-size: 1.4rem;
    overflow-wrap: break-word;
@@ -141,12 +141,12 @@ body{
    width:100%;
    border-top:1px solid black;
    margin-top:15px;
-   min-height:170px;
+   min-height:180px;
 }
 .topRows{
    width:100%;
    margin-top:15px;
-   min-height:170px;
+   min-height:180px;
 }
 .top-box{
    width: 100%;
